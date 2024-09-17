e5829364007e764a544909cbab446f89a612dfe0
Bart Stoffels <bart.stoffels@student.gildeopleidingen.nl>
2024-09-17 13:16:34 +0200

    folder structuur v2

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/style.css">
   <title>EasyEvents</title>
 </head>
 <body>
 	<?php require_once("./parts/nav.html"); ?>
 	<div class="main-container">
-
+		test content
 	</div>
 	<?php require_once("./parts/footer.html"); ?>
 </body>
