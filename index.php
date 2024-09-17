1cb361adc3e3917ea03ef6dfd64083bd878c52d0
Bart Stoffels <bart.stoffels@student.gildeopleidingen.nl>
2024-09-17 13:07:53 +0200

    structuur folder

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+  require_once("./DB_connection.php");
+  require_once("./functions/.");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>EasyEvents</title>
+</head>
+<body>
+	<?php require_once("./parts/nav.html"); ?>
+	<div class="main-container">
+
+	</div>
+	<?php require_once("./parts/footer.html"); ?>
+</body>
+</html>
