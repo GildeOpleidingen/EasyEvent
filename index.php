0eaf2ee9cb4303e257a2b0e0f4f581255b5feff9
Bart Stoffels <bart.stoffels@student.gildeopleidingen.nl>
2024-09-19 10:18:38 +0200

    favicon toegevoegd en urls aangepast

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
-  require_once("./functions/DB_connection.php");
-  require_once("./functions/functions.php");  
+  require_once("easyevents/functions/DB_connection.php");
+  require_once("easyevents/functions/functions.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./css/style.css">
+  <link rel="icon" href="easyevents/images/icons/favicon.png">
+  <link rel="stylesheet" href="easyevents/css/style.css">
   <title>EasyEvents</title>
 </head>
 <body>
-	<?php require_once("./parts/nav.html"); ?>
+	<?php require_once("easyevents/parts/nav.html"); ?>
 	<div class="main-container">
 		test content
 	</div>
-	<?php require_once("./parts/footer.html"); ?>
+	<?php require_once("easyevents/parts/footer.html"); ?>
 </body>
 </html>
