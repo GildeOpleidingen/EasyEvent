b70532daaf27aaebc07d596cae41f8310375abc7
Joep Verhofstad <joep.verhofstad@student.gildeopleidingen.nl>
2024-10-14 09:23:21 +0200

    Routing aangepast

--- src/Views/dashboard.php
+++ src/Views/dashboard.php
@@ -8,15 +8,15 @@ require_once './config/conn.php';
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- favicons -->
-    <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon">
-    <link rel="shortcut icon" href="./images/favicon-16x16.png" type="image/x-icon" sizes="16x16">
-    <link rel="shortcut icon" href="./images/favicon-32x32.png" type="image/x-icon" sizes="32x32">
+    <link rel="shortcut icon" href="../images/icons/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../images/icons/favicon-16x16.png" type="image/x-icon" sizes="16x16">
+    <link rel="shortcut icon" href="../images/icons/favicon-32x32.png" type="image/x-icon" sizes="32x32">
 
     <title>EasyEvents | Login</title>
 
     <!-- css -->
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
@@ -34,10 +34,10 @@ require_once './config/conn.php';
         <!-- tab 4 - Profiel -->
     </div>
 
-    <script src="./js/bootstrap.bundle.js"></script>
-    <script src="./js/script.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/script.js"></script>
     <script src="https://kit.fontawesome.com/a70ad4540c.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="./js/animaties.js"></script>
+    <script src="../js/animaties.js"></script>
 </body>
 </html>
