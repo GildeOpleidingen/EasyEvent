b28ed84283397877478d56eaf24de76a1fa1e7ce
Joep Verhofstad <joep.verhofstad@student.gildeopleidingen.nl>
2024-11-19 14:48:42 +0100

    Router profiel

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Controllers\BeheerEventAanmakenController;
+use App\Controllers\BeheerHomeController;
 use App\Controllers\HomeController;
 use App\Controllers\DashboardController;
 use App\Controllers\EventInfoController;
 use App\Controllers\LoginController;
 use App\Controllers\RegisterController;
 use App\Controllers\EventsController;
+use App\Controllers\ProfielController;
 use App\Router;
 
 $router = new Router();
@@ -19,6 +21,8 @@ $router->get('/register', RegisterController::class, 'index');
 $router->get('/events', EventsController::class, 'index');
 $router->get('/event-info', EventInfoController::class, 'index');
 $router->get('/beheer/event-aanmaken', BeheerEventAanmakenController::class, 'index');
+$router->get('/beheer/', BeheerHomeController::class, 'index');
+$router->get('/profiel', ProfielController::class, 'index');
 
 
 $router->dispatch();
