d01dbbe981beeec5c55067f2ad9ba8100ec5c17a
Joep Verhofstad <joep.verhofstad@student.gildeopleidingen.nl>
2025-01-22 09:15:58 +0100

    geen idee wat er veranderd is, maar er staat modified

--- src/Views/profiel.php
+++ src/Views/profiel.php
@@ -146,7 +146,6 @@ use App\Conn;
 
     <script src="../../js/bootstrap.bundle.js"></script>
     <script src="../../js/script.js"></script>
-    <script src="https://kit.fontawesome.com/a70ad4540c.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="../../js/searchbar.js"></script>
     <script src="../../js/tabs.js"></script>
