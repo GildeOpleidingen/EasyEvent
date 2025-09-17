9f5ac830ecd37d63950ea5e602d87c2897c48d12
naz <nazaslann1@gmail.com>
2025-09-17 11:57:02 +0200

    fix: Aligned navbar content with the sections

--- src/Views/events.php
+++ src/Views/events.php
@@ -91,7 +91,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="../../js/animaties.js"></script>
     <script src="../../js/searchbar.js"></script>
-    <script src="../../js/tabs.js"></script>
     <script src="../../js/calendar.js"></script>
     <script>
         function filterEvents() {
--- src/Views/profiel.php
+++ src/Views/profiel.php
@@ -195,7 +195,6 @@ use App\Conn;
     <script src="../../js/script.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="../../js/searchbar.js"></script>
-    <script src="../../js/tabs.js"></script>
     <script src="../../js/calendar.js"></script>
 </body>
 </html>
