e38e3cdc40e1a87873395b8e045e67dde2edb2d8
naz <nazaslann1@gmail.com>
2025-09-17 09:47:49 +0200

    fix: Configured Conn.php

--- src/Conn.php
+++ src/Conn.php
@@ -2,40 +2,44 @@
 
 namespace App
 {
+
     use PDO;
     use PDOException;
+    use Dotenv\Dotenv;
 
     class Conn
     {
         private static ?Conn $instance = null;
         private ?PDO $pdo = null;
 
-        
+        // Constructor to initialize the connection parameters
         private function __construct(){
-$host = $_ENV['DB_HOST'] ?? 'db';
-$dbname = $_ENV['DB_NAME'] ?? 'easyevent';
-$username = $_ENV['DB_USER'] ?? 'Admin';
-$password = $_ENV['DB_PASS'] ?? 'Admin';
+                $dotenv = Dotenv::createImmutable(__DIR__ . '/../', '.env');
+                $dotenv->load();
+                $host = $_ENV['DB_HOST'];
+                $dbname = $_ENV['DB_NAME'];
+                $username = $_ENV['DB_USER'];
+                $password = $_ENV['DB_PASS'];
 
-
-            try {
-                $this->pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password, [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-                ]);
-            } catch (PDOException $ex) {
-                echo "Database connection failed: " . $ex->getMessage();
-            }
+    try{
+        $this->pdo = new \PDO("mysql:host=$host;dbname=$dbname", $username, $password, [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+            ]);
+            } catch (PDOException $ex){
+                echo $ex->getMessage();
         }
+    }
 
-        public static function getInstance() : ?Conn {
+    public static function getInstance() : ?Conn {
             if (!self::$instance) {
-                self::$instance = new self();
-            }
-            return self::$instance;
+            self::$instance = new self();
         }
+        return self::$instance;
+    }
 
         public static function getPDO() : ?PDO {
             return self::getInstance()->pdo;
         }
     }
 }
+?>
