874c78b3642f9878ad8f77689e0275f2eab34e46
naz <nazaslann1@gmail.com>
2025-10-30 09:02:44 +0100

    mobile responsive

--- src/Views/add-child.php
+++ src/Views/add-child.php
@@ -1,4 +1,3 @@
-
 <?php
 use App\Conn;
 ?>
@@ -8,14 +7,12 @@ use App\Conn;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- favicons -->
     <link rel="shortcut icon" href="/images/icons/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="/images/icons/favicon-16x16.png" type="image/x-icon" sizes="16x16">
     <link rel="shortcut icon" href="/images/icons/favicon-32x32.png" type="image/x-icon" sizes="32x32">
 
     <title>EasyEvents | Kind Toevoegen</title>
 
-    <!-- css -->
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/login.css">
@@ -28,7 +25,8 @@ use App\Conn;
     <?php require_once(__ROOT__."/parts/nav.html"); ?>
     <div class="d-flex justify-content-center align-items-center h-75">
         <div class="row">
-            <div class="col-lg-6 d-flex align-items-center justify-content-center">
+            <?php //?>
+            <div class="col-lg-6 d-none d-lg-flex align-items-center justify-content-center">
                 <img src="../../images/logo.png" alt="Easy Events logo" class="logo-img">
             </div>
             <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center">
@@ -47,6 +45,8 @@ use App\Conn;
                 </div>
                 <?php endif; ?>
 
+                <?php //?>
+                <?php if (isset($gebruiker)): ?>
                 <form action="/add-child" method="POST" class="w-75">
                     <div class="form-floating mb=3">
                         <input type="text" id="voornaam" name="voornaam" class="form-control rounded-0" placeholder="Voornaam" required>
@@ -60,26 +60,40 @@ use App\Conn;
                         <input type="date" id="geboortedatum" name="geboortedatum" class="form-control rounded-0" placeholder="Geboortedatum" required>
                         <label for="geboortedatum">Geboortedatum</label>
                     </div>
+
+                    <p class="text-muted text-center ms-6">Adresgegevens (standaard van jou, pas aan indien nodig)</p>
+
                     <div class="row mb-3">
                         <div class="col-md-6">
                             <div class="form-floating mb-3">
-                                <input type="text" id="postcode" name="postcode" class="form-control rounded-0" placeholder="Postcode" required>
+                                <?php //?>
+                                <input type="text" id="postcode" name="postcode" class="form-control rounded-0" placeholder="Postcode"
+                                       value="<?php echo htmlspecialchars($gebruiker->getPostcode() ?? ''); ?>" required>
                                 <label for="postcode">Postcode</label>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-floating mb-3">
-                                <input type="text" id="woonplaats" name="woonplaats" class="form-control rounded-0" placeholder="Woonplaats" required>
-                                <label for="woonplaats">Woonplaats</label>
+                                <?php //?>
+                                <input type="text" id="plaatsnaam" name="plaatsnaam" class="form-control rounded-0" placeholder="Plaatsnaam"
+                                       value="<?php echo htmlspecialchars($gebruiker->getPlaatsnaam() ?? ''); ?>" required>
+                                <label for="plaatsnaam">Plaatsnaam</label>
                             </div>
                         </div>
-                            <div class="form-floating mb-3">
-                                <input type="text" id="adress" name="adress" class="form-control rounded-0" placeholder="Adres" required>
-                                <label for="adress">Adres</label>
-                            </div>
+                        <div class="form-floating mb-3">
+                            <?php //?>
+                            <input type="text" id="huisnummer" name="huisnummer" class="form-control rounded-0" placeholder="Huisnummer"
+                                   value="<?php echo htmlspecialchars($gebruiker->getHuisnummer() ?? ''); ?>" required>
+                            <label for="huisnummer">Huisnummer</label>
+                        </div>
                     </div>
                     <button class="btn btn-primary w-100 login-btn mt-3" type="submit">Kind Toevoegen</button>
                 </form>
+                <?php else: ?>
+                <?php //?>
+                <div class="alert alert-warning">Kon gebruikersgegevens niet laden. <a href="/profiel">Ga terug</a>.</div>
+                <?php endif; ?>
+
                 <p class="text-center ms-6 mt-3"><a href="/profiel">Terug naar profiel</a></p>
             </div>
         </div>
